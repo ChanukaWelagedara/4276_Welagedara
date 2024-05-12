b9d0d7c653d4f5a3bf908dc1d2d37fb91408e600
chanuka <chanukawelagedara@gmail.com>
2024-05-13 00:14:43 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,21 +1,48 @@
-pipeline {
-  agent any
-  stages {
-    stage('build'){
-      steps {
-        sh 'docker build -t node-image .'
-      }
-    }
-    stage('run'){
-      steps{
-        sh 'docker run -d -p 5000:3000 node-image'
-      }
-    }
-    stage('final'){
-      steps{
-        sh 'docker ps'
-      }
-    }
-  }
+// pipeline {
+//   agent any
+//   stages {
+//     stage('build'){
+//       steps {
+//         sh 'docker build -t node-image .'
+//       }
+//     }
+//     stage('run'){
+//       steps{
+//         sh 'docker run -d -p 5000:3000 node-image'
+//       }
+//     }
+//     stage('final'){
+//       steps{
+//         sh 'docker ps'
+//       }
+//     }
+//   }
 
+// }
+
+
+pipeline {
+    agent any
+    stages {
+        stage('Docker Build') {
+            steps {
+                sh 'docker build -t node-image .'
+            }
+        }
+        stage('Docker Run') {
+            steps {
+                sh 'docker run -d -p 5000:3000 node-image'
+            }
+        }
+        stage('Final') {
+            steps {
+                sh 'docker ps'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying application...'
+            }
+        }
+    }
 }
