ded206f8b2c56b4a113370318dcf50aeb26e3f0e
chanuka <chanukawelagedara@gmail.com>
2024-05-13 00:21:24 +0530

    9

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,25 +1,3 @@
-// pipeline {
-//   agent any
-//   stages {
-//     stage('build'){
-//       steps {
-//         sh 'docker build -t node-image .'
-//       }
-//     }
-//     stage('run'){
-//       steps{
-//         sh 'docker run -d -p 5000:3000 node-image'
-//       }
-//     }
-//     stage('final'){
-//       steps{
-//         sh 'docker ps'
-//       }
-//     }
-//   }
-
-// }
-
 
 pipeline {
     agent any
